2e5ddade4ca46923c51b1c5d49808991d1906f1e
henrique <hrqneto@gmail.com>
2023-11-12 22:40:49 -0300

    fix: refactor request service for enhanced efficiency and error handling and style

--- src/api/githubAPI.ts
+++ src/api/githubAPI.ts
@@ -1,20 +1,22 @@
 import axios from "axios";
-import { UserData } from "../types";
+
+const GITHUB_API_BASE_URL = "https://api.github.com";
 
 const githubAPI = axios.create({
-  baseURL: "https://api.github.com",
+  baseURL: GITHUB_API_BASE_URL,
 });
-
-//const TIMEOUT_MS = 10000;
 
 export async function getUserDetailsAndRepos(username: string) {
   try {
-    const userDetails = await githubAPI.get<UserData>(`/users/${username}`);
-    const userRepos = await githubAPI.get(`/users/${username}/repos`);
-    return {
-      userDetails: userDetails.data,
-      userRepos: userRepos.data,
-    };
+    const [userDetailsResponse, userReposResponse] = await Promise.all([
+      githubAPI.get(`/users/${username}`),
+      githubAPI.get(`/users/${username}/repos`),
+    ]);
+
+    const userDetails = userDetailsResponse.data;
+    const userRepos = userReposResponse.data;
+
+    return { userDetails, userRepos };
   } catch (error) {
     console.error("Error fetching user details and repositories:", error);
     throw error;
